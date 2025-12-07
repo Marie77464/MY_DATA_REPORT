7c07bfa1fdd26130b945209eeb67fac0eb410a74
Marie77464 <marie.p.basse@aims-senegal.org>
2025-12-07 18:27:57 +0000

    Fix selectors for scraping car listings

--- my_data_app.py
+++ my_data_app.py
@@ -17,40 +17,38 @@ if st.button("Scraper"):
     with st.spinner("Scraping en cours..."):
         for index in range(1, M+1):
 
-            # URL CORRIGÉE (page variable)
             url = f'https://dakar-auto.com/senegal/voitures-4?page={index}'
-
             res = get(url)
             soup = bs(res.content, 'html.parser')
 
-            # SELECTEUR CORRIGÉ
-            containers = soup.find_all('div', class_='listings-cards__list-item')
+            # NOUVEAU SELECTEUR QUI MARCHE !
+            containers = soup.find_all('div', class_='listing-card')
 
             data = []
             for container in containers:
                 try:
-                    # TITLE
-                    title_block = container.find('h2')
-                    gen_inf = title_block.text.strip().split()
+                    # TITRE
+                    title = container.find('h2', class_='listing-card__title')
+                    gen_inf = title.text.strip().split()
 
                     brand = gen_inf[0]
                     model = " ".join(gen_inf[1:len(gen_inf)-1])
                     year = gen_inf[-1]
 
-                    # ATTRIBUTES
-                    gen_inf1 = container.find('ul')
-                    gen_inf2 = gen_inf1.find_all('li')
+                    # CARACTÉRISTIQUES
+                    attrs = container.find('ul', class_='listing-card__attributes')
+                    li = attrs.find_all('li')
 
-                    kms_driven = gen_inf2[1].text.replace('km', '') if len(gen_inf2) > 1 else "N/A"
-                    gearbox = gen_inf2[2].text if len(gen_inf2) > 2 else "N/A"
-                    fuel_type = gen_inf2[3].text if len(gen_inf2) > 3 else "N/A"
+                    kms_driven = li[1].text.replace("km", "") if len(li) > 1 else "N/A"
+                    gearbox = li[2].text if len(li) > 2 else "N/A"
+                    fuel_type = li[3].text if len(li) > 3 else "N/A"
 
-                    # OWNER
-                    owner_block = container.find('p')
+                    # PROPRIÉTAIRE
+                    owner_block = container.find('p', class_='listing-card__author')
                     owner = owner_block.text.replace("Par", "").strip() if owner_block else "N/A"
 
-                    # PRICE
-                    price_block = container.find('h3')
+                    # PRIX (NOUVEAU SELECTEUR)
+                    price_block = container.find('div', class_='listing-card__price')
                     price = price_block.text.replace("FCFA", "").strip() if price_block else "N/A"
 
                     dic = {
@@ -75,9 +73,4 @@ if st.button("Scraper"):
     st.dataframe(df)
 
     csv = df.to_csv(index=False)
-    st.download_button(
-        "Télécharger le CSV", 
-        data=csv, 
-        file_name="annonces.csv", 
-        mime="text/csv"
-    )
+    st.download_button("Télécharger le CSV", data=csv, file_name="annonces.csv", mime="text/csv")
