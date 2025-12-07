81bad30268f32937e77d76186d828f22dc189821
Marie77464 <marie.p.basse@aims-senegal.org>
2025-12-07 03:02:25 +0000

    Implement web scraper for car announcements
    Added web scraping functionality for car listings.

--- my_data_app.py
+++ my_data_app.py
@@ -1,47 +1,73 @@
 import streamlit as st
 import pandas as pd
+from bs4 import BeautifulSoup as bs
+from requests import get
 
+def num_page():
+    N = int(st.number_input("Combien de pages voulez-vous scraper ?", min_value=1, value=1, step=1))
+    return N
 
-st.markdown("<h1 style='text-align: center; color: black;'>MY DATA APP</h1>", unsafe_allow_html=True)
+st.title("Scraper annonces voitures")
 
-st.markdown("""
-This app allows you to download scraped data on motocycles from expat-dakar 
-* **Python libraries:** base64, pandas, streamlit
-* **Data source:** [Expat-Dakar](https://www.expat-dakar.com/).
-""")
+M = num_page()
 
+df = pd.DataFrame()
 
-# Function for loading the data
-def load_(dataframe, title, key) :
-    st.markdown("""
-    <style>
-    div.stButton {text-align:center}
-    </style>""", unsafe_allow_html=True)
+if st.button("Scraper"):
+    with st.spinner("Scraping en cours..."):
+        # loop over pages indexes
+        for index in range(1, M+1):
+            url = 'https://dakar-auto.com/senegal/voitures-4'
+            # get the html code of the page using the get function requests
+            res = get(url)
+            res
+            # store the html code dans un objet BeautifulSoup avec html parser
+            soup = bs(res.content, 'html.parser')
+            # get all containers that contains the informations of each car
+            containers = soup.find_all('div', class_='listings-cards__list-item mb-md-3 mb-3')
+            # scrape data from all the containers
+            data = []
+            for container in containers:
+                try:
+                    gen_inf = container.find('h2', class_='listing-card__header__title mb-md-2 mb-0').a.text.strip().split()
+                    # get the brand
+                    brand = gen_inf[0]
+                    # get the model
+                    model = " ".join(gen_inf[1:len(gen_inf)-1])
+                    # get the year
+                    year = gen_inf[-1]
+                    # Scrape the kilometer, fuel type and gearbox type
+                    gen_inf1 = container.find('ul', 'listing-card__attribute-list list-inline mb-0')
+                    gen_inf2 = gen_inf1.find_all('li', 'listing-card__attribute list-inline-item')
+                    # get kilometer driven
+                    kms_driven = gen_inf2[1].text.replace('km', '')
+                    # get the gearbox
+                    gearbox = gen_inf2[2].text
+                    # get fuel type
+                    fuel_type = gen_inf2[3].text
+                    # scrape owner
+                    owner = "".join(container.find('p', class_='time-author m-0').a.text).replace('Par', '')
+                    # scrape the price
+                    price = "".join(container.find('h3', 'listing-card__header__price font-weight-bold text-uppercase mb-0').text.strip().split()).replace('FCFA', '')
+                    dic = {
+                        "brand": brand,
+                        "model": model,
+                        "year": year,
+                        "kilometer": kms_driven,
+                        "fuel_type": fuel_type,
+                        "gearbox": gearbox,
+                        "owner": owner,
+                        "price": price,
+                    }
+                    data.append(dic)
+                except:
+                    pass
+            DF = pd.DataFrame(data)
+            df = pd.concat([df, DF], axis=0).reset_index(drop=True)
 
-    if st.button(title,key):
-      
-        st.subheader('Display data dimension')
-        st.write('Data dimension: ' + str(dataframe.shape[0]) + ' rows and ' + str(dataframe.shape[1]) + ' columns.')
-        st.dataframe(dataframe)
+    st.success("Scraping terminé !")
+    st.dataframe(df)
 
-# define some styles rely to the box
-st.markdown('''<style> .stButton>button {
-    font-size: 12px;
-    height: 3em;
-    width: 25em;
-}</style>''', unsafe_allow_html=True)
-
-          
-# load the data
-load_(pd.read_csv('data/motos_scooters1.csv'), 'Motocycles data 1', '1')
-load_(pd.read_csv('data/motos_scooters2.csv'), 'Motocycles data 2', '2')
-load_(pd.read_csv('data/motos_scooters3.csv'), 'Motocycles data 3', '3')
-load_(pd.read_csv('data/motos_scooters4.csv'), 'Motocycles data 4', '4')
-load_(pd.read_csv('data/motos_scooters5.csv'), 'Motocycles data 5', '5')
-
-
-
-
- 
-
-
+    # Optionnel : proposer de télécharger le résultat en CSV
+    csv = df.to_csv(index=False)
+    st.download_button("Télécharger le CSV", data=csv, file_name="annonces.csv", mime="text/csv")
